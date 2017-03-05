9a26665898edea58174f742e729c9d1c40eb3091
Kevin Finley <kftwotwo@gmail.com>
2017-03-05 14:47:08 -0800

    add method to create leads

--- app/models/insightly.rb
+++ app/models/insightly.rb
@@ -2,8 +2,8 @@ class Insightly < ApplicationRecord
   Insightly2.api_key = ENV['INSIGHTLY_API_KEY']
 
   class << self
-    def get_contacts
-      Insightly2.client.get_contacts
+    def create_lead(first_name, last_name, phone_number, email)
+      Insightly2.client.create_lead(:lead => {:first_name => first_name, :last_name => last_name, :phone_number => phone_number, :email_address => email})
     end
   end
 end
--- spec/models/insightly_spec.rb
+++ spec/models/insightly_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Insightly, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Insightly, :vcr => true do
+  before :each do
+    Insightly2.api_key = ENV['INSIGHTLY_API_KEY']
+  end
+
+  # it "create a lead" do
+  #   Insightly.create_lead("first_name", "last_name", "phone_number", "email")
+  #   expect(lead.status).to eq 200
+  # end
 end
