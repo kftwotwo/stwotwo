ba90ce581cd31d7c196baad72f6ddef8c7f2ec48
Kevin Finley <kftwotwo@gmail.com>
2017-03-05 22:48:23 -0800

    STS-7: add specs for the controller

--- spec/controllers/leads_controller_spec.rb
+++ spec/controllers/leads_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe LeadsController, type: :controller do
+describe LeadsController do
 
+  describe "GET new" do
+    it "renders a 200 response" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new category" do
+      get :new
+      expect(assigns(:lead)).to be_a_new(Lead)
+    end
+  end
+
+  describe "POST create", :vcr => true do
+    it "renders a 302 response" do
+      post :create, :lead => {:first_name => "test", :last_name => "test", :phone_number => "test", :email => "test"}
+      expect(response.status).to eq(302)
+    end
+
+    it "renders the new if a field is empty" do
+      post :create, :lead => {:last_name => "test", :phone_number => "test", :email => "test"}
+      expect(response).to render_template(:new)
+    end
+  end
 end
--- spec/controllers/organisations_controller_spec.rb
+++ spec/controllers/organisations_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe OrganisationsController, type: :controller do
+describe OrganisationsController do
 
+  describe "GET new" do
+    it "renders a 200 response" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new category" do
+      get :new
+      expect(assigns(:organisation)).to be_a_new(Organisation)
+    end
+  end
+
+  describe "POST create", :vcr => true do
+    it "renders a 302 response" do
+      post :create, :organisation => {:name_of_organisation => "test", :description_of_organisation => "test", :organisation_email => "test", :organisation_phone => "test"}
+      expect(response.status).to eq(302)
+    end
+
+    it "renders a 200 if a field is empty" do
+      post :create, :organisation => {:name_of_organisation => "test", :organisation_email => "test", :organisation_phone => "test"}
+      expect(response).to render_template(:new)
+    end
+  end
 end
