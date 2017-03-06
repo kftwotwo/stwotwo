8c3a365699fb623e16ccd37531148817d5ee807f
Kevin Finley <kftwotwo@gmail.com>
2017-03-05 16:29:55 -0800

    STS-7: add method to create organisation

--- app/models/insightly.rb
+++ app/models/insightly.rb
@@ -5,5 +5,14 @@ class Insightly < ApplicationRecord
     def create_lead(first_name, last_name, phone_number, email)
       Insightly2.client.create_lead(:lead => {:first_name => first_name, :last_name => last_name, :phone_number => phone_number, :email_address => email})
     end
+
+    def  create_organisation(name, description, phone, email)
+      Insightly2.client.create_organisation(:organisation => {
+        :organisation_name => name,
+        :background => description,
+          :work_email => email,
+          :contactinfos => [{:type =>"Phone", :subtype =>"", :label => "Work", :detail => phone}]
+        })
+    end
   end
 end
