780b4f8b054b5ce487e7e0e33d456c9f05e79630
Kevin Finley <kftwotwo@gmail.com>
2017-03-05 22:14:18 -0800

    STS-7: allow user to create a organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,7 +1,21 @@
 class OrganisationsController < ApplicationController
 
   def new
+    @organisation = Organisation.new
+  end
 
+  def create
+    @organisation = Organisation.new(insightly_params_organisation)
+    if @organisation.save
+      name_of_organisation = insightly_params_organisation['name_of_organisation']
+      description_of_organisation = insightly_params_organisation['description_of_organisation']
+      organisation_email = insightly_params_organisation['organisation_email']
+      organisation_phone = insightly_params_organisation['organisation_phone']
+      Organisation.create_organisation(name_of_organisation, description_of_organisation, organisation_email, organisation_phone)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
