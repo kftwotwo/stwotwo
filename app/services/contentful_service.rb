5680d8eab15f4d822b0f79222b9b70680ea915ae
Kevin Finley <kftwotwo@gmail.com>
2017-03-08 10:09:58 -0800

    STS-33: use contentful and display blogs

--- app/services/contentful_service.rb
+++ app/services/contentful_service.rb
@@ -1,15 +1,16 @@
-files = File.join(File.dirname(__FILE__),'contentful','**','*.rb')
-Dir.glob(files).each do |file|
-  require file
-end
-
-require 'contentful'
-
-module ContentfulService
+class ContentfulService
   @client ||= Contentful::Client.new(
     access_token: ENV['CONTENTFUL_DELIVERY_API'],
     space: ENV['CONTENTFUL_SPACE_ID'],
     dynamic_entries: :auto,
     raise_errors: true
   )
+
+  class << self
+    def get_blogs
+      blogs = @client.entries
+       blog_json = blogs.to_json
+      JSON.parse(blog_json)
+    end
+  end
 end
