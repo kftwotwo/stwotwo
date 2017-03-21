9ee16ffe1d77d370e280471dc30fff1b2e455a2c
Kevin Finley <kftwotwo@gmail.com>
2017-03-20 20:25:13 -0700

    STS-51: trying to make responsive

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   $('.carousel.carousel-slider').carousel({fullWidth: true});
-  window.setInterval(function(){$('.carousel').carousel('next')},3000)
+  // window.setInterval(function(){$('.carousel').carousel('next')},3000)
 });
