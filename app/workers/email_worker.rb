9524479a084d58ffd3e3c0506c831e371566917f
Kevin Finley <kftwotwo@gmail.com>
2017-04-05 22:01:19 -0700

    STS-52: add graceful error handling for sendgrid

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,8 +1,14 @@
 class EmailWorker
   include Sidekiq::Worker
+  require 'rollbar'
 
   def perform(lead_id)
+    begin
     lead = Lead.find(lead_id)
     OnBoardMailer.welcome_email(lead).deliver
+    rescue Exception => e
+      logger.error e.message
+      Rollbar.error(e)
+    end
   end
 end
